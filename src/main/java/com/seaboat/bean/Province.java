e71acb5b781ca0bcee077fe8b217da79ac4050bb
a-w-a-n-g <a00wang@163.com>
2020-05-25 13:50:40 +0800

    pass messages

---
+++ src/main/java/com/seaboat/bean/Province.java
@@ -0,0 +1,23 @@
+package com.seaboat.bean;
+
+/**
+
+* @author awang
+* @version 1.0
+* @date 2020年5月25日 下午1:47:40
+* 
+*/
+
+public class Province {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
--- src/main/java/com/seaboat/controller/MessageController.java
+++ src/main/java/com/seaboat/controller/MessageController.java
@@ -1,9 +1,12 @@
 package com.seaboat.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.seaboat.bean.Province;
 
 /**
 
@@ -21,9 +24,14 @@ public class MessageController {
 		return "message";
 	}
 	
-	@RequestMapping(value = "/setProvince/{province}", method = RequestMethod.POST)
+	@RequestMapping(value = "/setProvince/{province}", method = RequestMethod.GET)
 	public String setProvince(@PathVariable("province") String province) {
 		return province;
+	}
+	
+	@RequestMapping(value = "/setProvince", method = RequestMethod.POST)
+	public String setProvince(@RequestBody Province province) {
+		return province.getName();
 	}
 
 }
